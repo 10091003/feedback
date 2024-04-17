1fa6b3b15a2f86198237795117d8488eb9ee9473
Cezar Mariano <cezardimariano@hotmail.com>
2024-04-16 21:36:01 -0300

    Criando 4 parametros para Escola de Dança

---
+++ src/main/java/com/estudo/feedback/api3/request/DanceRequest.java
@@ -0,0 +1,9 @@
+package com.estudo.feedback.api3.request;
+
+public class DanceRequest {
+    //Criando 4 parametros para objeto Dança
+    private String Sala;
+    private String Professor;
+    private String Estilo;
+    private String Alunos;
+}
