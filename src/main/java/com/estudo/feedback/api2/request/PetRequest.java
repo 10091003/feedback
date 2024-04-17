680bfb2c0f72a8fbc28af55a760915085d97cc10
Cezar Mariano <cezardimariano@hotmail.com>
2024-04-16 21:26:55 -0300

    Criando cinco parametros para Pet

---
+++ src/main/java/com/estudo/feedback/api2/request/PetRequest.java
@@ -0,0 +1,10 @@
+package com.estudo.feedback.api2.request;
+
+public class PetRequest {
+    //Criar cinco paramentros para Objeto Pet
+    private String pet;
+    private String nome;
+    private String raça;
+    private String idade;
+    private String endereço;
+}
